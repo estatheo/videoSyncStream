e075765cf9b46566b47bfc39de60012e60287a4d
Theodor Chichirita <theodor.chichirita@coxauto.co.uk>
2017-10-03 02:11:07 +0100

    changes for hosted in azure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app=require('express')();
 var http= require('http').Server(app);
 var io= require('socket.io')(http);
-
+var port = process.env.PORT || 3000
 
 app.get('/', function(req,res){
     res.sendFile(__dirname+ '/index.html');
@@ -20,7 +20,7 @@ io.on('connection', function(socket){
         socket.broadcast.emit('command', command);
     })
   });
-http.listen(80, function(){
+app.listen(process.env.port, function(){
     
 });
 
